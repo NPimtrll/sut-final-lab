ce1bea40ac35c762b82e50fe32550a7f8b1ec992
Admin <NannapatSrimartpirom@gmail.com>
2024-02-01 14:16:18 +0700

    close #4

--- backend/entity/Customers.go
+++ backend/entity/Customers.go
@@ -7,6 +7,6 @@ import (
 type Customers struct {
 	gorm.Model
 	Name       string `valid:"required~Need Name"`
-	Age        string //`valid:"stringlentgh(0|2)~Age is not integer"`
-	CustomerID string //`valid:"matches(^[C]\\s[MU]\\d{8}$)~ComtomerID is not pattern"`
+	Age        string //`valid:"stringlength(0|2)~Age is not integer"`
+	CustomerID string `valid:"matches(^[C]\\s[MU]\\d{8}$)~ComtomerID is not pattern"`
 }
